de8dcb0790c7fcbed22e1fd7c008e1eb9c705617
Filipe Paz <filipe_paz.28@hotmail.com>
2021-07-26 21:30:35 -0300

    Pequena mudança no menu e nova seção

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-var slideIndex = 1;
+// Slideshow
+var slideIndex = 0;
 showDivs(slideIndex);
 
 function plusDivs(n) {
@@ -23,4 +24,38 @@ function showDivs(n) {
   }
   x[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " w3-white";
+}
+
+// Passar imagem automaticamente a cada 4s
+auto();
+
+function auto() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1}    
+  x[slideIndex-1].style.display = "block";  
+  setTimeout(auto, 4000);
+  showDivs(slideIndex);    
+}
+
+// Alternancia de menu
+function myFunction() {
+  var x = document.getElementById("navDemo");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+
+// Clicar na tela para fechar
+var modal = document.getElementById('ticketModal');
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
