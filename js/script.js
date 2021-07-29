362f0755678f195ddf8e292d8a406070244ef575
Filipe Paz <filipe_paz.28@hotmail.com>
2021-07-28 23:28:01 -0300

    Subindo correção de bugs

--- js/script.js
+++ js/script.js
@@ -59,3 +59,66 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+/* var slideIndex = [1,1];
+var slideId = ["mySlides1", "mySlides2"];
+showDivs(1, 0);
+showDivs(1, 1);
+
+function plusDivs(n, no) {
+  showDivs(slideIndex[no] += n, no);
+}
+
+function currentDiv(n) {
+  showDivs(slideIndex[0] = n, 0);
+}
+
+function showDivs(n, no) {
+  var i;
+  var x = document.getElementsByClassName(slideId[no]);
+  var dots = document.getElementsByClassName("dots");
+  if (n > x.length) {slideIndex[no] = 1}
+  if (n < 1) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" w3-white", "");
+  }
+  x[slideIndex[no]-1].style.display = "block";
+  dots[slideIndex[0]-1].className += " w3-white";
+}
+
+// Passar imagem automaticamente a cada 4s
+auto();
+
+function auto() {
+  var i;
+  var x = document.getElementsByClassName("mySlides1");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1}    
+  x[slideIndex-1].style.display = "block";  
+  setTimeout(auto, 4000);
+  showDivs(slideIndex);    
+}
+
+// Alternancia de menu
+function myFunction() {
+  var x = document.getElementById("navDemo");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+
+// Clicar para fechar
+var modal = document.getElementById('ticketModal');
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+} */
