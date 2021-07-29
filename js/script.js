cdb49bc0abd7a176b44ebaefc8e37b5aa51b1417
Filipe Paz <filipe_paz.28@hotmail.com>
2021-07-29 01:11:03 -0300

    Adicionando kits e correções

--- js/script.js
+++ js/script.js
@@ -1,69 +1,11 @@
 // Slideshow
-var slideIndex = 0;
-showDivs(slideIndex);
-
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
-
-function currentDiv(n) {
-  showDivs(slideIndex = n);
-}
-
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demodots");
-  if (n > x.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = x.length} ;
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" w3-white", "");
-  }
-  x[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " w3-white";
-}
-
-// Passar imagem automaticamente a cada 4s
-auto();
-
-function auto() {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  slideIndex++;
-  if (slideIndex > x.length) {slideIndex = 1}    
-  x[slideIndex-1].style.display = "block";  
-  setTimeout(auto, 4000);
-  showDivs(slideIndex);    
-}
-
-// Alternancia de menu
-function myFunction() {
-  var x = document.getElementById("navDemo");
-  if (x.className.indexOf("w3-show") == -1) {
-    x.className += " w3-show";
-  } else { 
-    x.className = x.className.replace(" w3-show", "");
-  }
-}
-
-// Clicar para fechar
-var modal = document.getElementById('ticketModal');
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-
-/* var slideIndex = [1,1];
-var slideId = ["mySlides1", "mySlides2"];
+var slideIndex = [1,1,1,1,1];
+var slideId = ["mySlides1", "mySlides2", "mySlides3", "mySlides4", "mySlides5"];
 showDivs(1, 0);
 showDivs(1, 1);
+showDivs(1, 2);
+showDivs(1, 3);
+showDivs(1, 4);
 
 function plusDivs(n, no) {
   showDivs(slideIndex[no] += n, no);
@@ -89,36 +31,10 @@ function showDivs(n, no) {
   dots[slideIndex[0]-1].className += " w3-white";
 }
 
-// Passar imagem automaticamente a cada 4s
-auto();
-
-function auto() {
-  var i;
-  var x = document.getElementsByClassName("mySlides1");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  slideIndex++;
-  if (slideIndex > x.length) {slideIndex = 1}    
-  x[slideIndex-1].style.display = "block";  
-  setTimeout(auto, 4000);
-  showDivs(slideIndex);    
-}
-
-// Alternancia de menu
-function myFunction() {
-  var x = document.getElementById("navDemo");
-  if (x.className.indexOf("w3-show") == -1) {
-    x.className += " w3-show";
-  } else { 
-    x.className = x.className.replace(" w3-show", "");
-  }
-}
-
 // Clicar para fechar
 var modal = document.getElementById('ticketModal');
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-} */
+}
